3a993351cda4cd6fd999fde31682a2cb0e74be1a
Hugo Marcotte <marcotte.hugo@gmail.com>
2014-11-26 12:21:02 -0500

    Add layout

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    app.set 
+    app.set
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
